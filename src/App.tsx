51a48c5e7d1bf43e6e8edea741da5974ed2c81e3
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-07 09:25:07 +0300

    Add: search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  {
-    fetch('http://localhost:5000?path=recipes/complexSearch?minProtein=20', {
+  useEffect(  ()=>{
+     fetch('http://localhost:5000?path=recipes/complexSearch?titleMatch=beef&number=1', {
       method: 'GET',
       headers: {
       }
@@ -17,30 +15,18 @@ function App() {
         console.error('Error:', error);
       });
 
-  }
+  },[])
+    
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
+    <div className="container">
+      <h1>Search recipes by using the Spoonacular API</h1>
+      <form action="">
+        <input type="text" name="" id="" placeholder='Search'/>
+        <input type="submit" value="Search API" />
+      </form>
+    </div>
     </>
   )
 }
